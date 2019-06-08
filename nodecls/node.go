cbeb1c26289105cf681e77a8131a9ac1a81108a5
J Fixby <github@jfixby.com>
2019-06-08 17:05:15 +0200

    regtest

--- nodecls/node.go
+++ nodecls/node.go
@@ -81,8 +81,8 @@ func networkFor(net coinharness.Network) string {
 	if net == &chaincfg.TestNet3Params {
 		return "testnet"
 	}
-	if net == &chaincfg.RegNetParams {
-		return "regnet"
+	if net == &chaincfg.RegressionNetParams {
+		return "regtest"
 	}
 	if net == &chaincfg.MainNetParams {
 		// no argument needed for the MainNet
